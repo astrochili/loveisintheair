93fd189e5f8770b73016285890a9de80cbeae1b0
astrochili <astrochili@users.noreply.github.com>
2017-03-30 10:10:30 +0300

    Steam improvement

--- classes/player.lua
+++ classes/player.lua
@@ -12,17 +12,13 @@ function Player:new(x, y, r)
   self.collider.fixtures.main:setRestitution(0.9)
   self.speed = 3000
 
+  self.steam = love.graphics.newParticleSystem(love.graphics.newImage('assets/images/circle.png'), 32)
+  self.steam:setParticleLifetime(1, 2)
+  self.steam:setColors(255, 255, 255, 255, 255, 255, 255, 0)
   gameloop:addLoop(self)
-
-  local img = love.graphics.newImage('assets/images/circle.png')
-  psystem = love.graphics.newParticleSystem(img, 32)
-  psystem:setParticleLifetime(1, 2)
-  psystem:setColors(255, 255, 255, 255, 255, 255, 255, 0)
-  gameloop:addLoop(psystem)
 end
 
 function Player:update(dt)
-
   pushX, pushY = 0, 0
 
   if love.keyboard.isDown("right") then
@@ -41,11 +37,12 @@ function Player:update(dt)
     self:push(dt, pushX, pushY)
   end
 
+  self.steam:update(dt)
 end
 
 function Player:draw()
   love.graphics.setColor(lume.color(colors.air, 256))
-  love.graphics.draw(psystem, self:getX(), self:getY())
+  love.graphics.draw(self.steam)
   love.graphics.setColor(lume.color(colors.ball, 256))
   love.graphics.circle("fill", self:getX(), self:getY(), self:getRadius())
 end
@@ -83,19 +80,20 @@ end
 function Player:push(dt, x, y)
   self.collider.body:applyForce(x, y) -- push ball
 
-  accelerationX = -x/self.speed * 200
-  accelerationY = -y/self.speed * 200
-  psystem:setSizes(self:getRadius()/16, self:getRadius()/32)
-  psystem:setLinearAcceleration(accelerationX-50, accelerationY-50, accelerationX+50, accelerationY+50)
-  psystem:emit(32)
-
   newRadius = self:getRadius() - dt * 2 -- release air
-  if newRadius < 1 then
-    newRadius = 1
-  end
-
+  if newRadius < 1 then newRadius = 1 end
   self:setRadius(newRadius)
 
+  self:releaseSteam(x, y)
+end
+
+function Player:releaseSteam(x, y)
+  accelerationX = -x/self.speed * 200
+  accelerationY = -y/self.speed * 200
+  self.steam:setSizes(self:getRadius()/16, self:getRadius()/32)
+  self.steam:setPosition(self:getX(), self:getY())
+  self.steam:setLinearAcceleration(accelerationX-50, accelerationY-50, accelerationX+50, accelerationY+50)
+  self.steam:emit(32)
 end
 
 return Player
